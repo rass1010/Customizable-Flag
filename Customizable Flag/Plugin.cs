1c24373a05ff3119dc50d84c06431a725e9f8478
rass1010 <rasmusberthag@gmail.com>
2024-06-06 16:05:22 +0200

    Add project files.

---
+++ Customizable Flag/Plugin.cs
@@ -0,0 +1,49 @@
+﻿using BepInEx;
+using System;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+using Utilla;
+
+namespace Customizable_Flag
+{
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    [BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")] // Make sure to add Utilla 1.5.0 as a dependency!
+    public class Plugin : BaseUnityPlugin
+    {
+
+        public static Plugin Instance { get; private set; }
+
+        void Awake()
+        {
+            Utilla.Events.GameInitialized += GameInitialized;
+            Instance = this;
+        }
+
+        private void GameInitialized(object sender, EventArgs e)
+        {
+            
+            // Player instance has been created
+            Stream str = Assembly.GetExecutingAssembly().GetManifestResourceStream("Customizable_Flag.flag");
+            AssetBundle bundle = AssetBundle.LoadFromStream(str);
+
+            GameObject Flag = bundle.LoadAsset<GameObject>("Flag");
+            GameObject newFlag = Instantiate(Flag);
+
+            Transform rightHand = GameObject.Find("Local Gorilla Player/rig/body/shoulder.R/upper_arm.R/forearm.R/hand.R/palm.01.R").transform;
+            newFlag.transform.SetParent(rightHand, false);
+            newFlag.transform.localScale = new Vector3(10f, 10f, 10f);
+            newFlag.transform.localEulerAngles = new Vector3(281.051117f, 253.24057f, 98.3139038f);
+            newFlag.transform.localPosition = new Vector3(0.0340000018f, 0.0250000004f, -0.451000005f);
+            newFlag.transform.Find("FlagCloth").gameObject.AddComponent<ResizeFlag>();
+            newFlag.transform.Find("Cylinder").GetComponent<Collider>().enabled = false;
+        }
+
+
+        public void log(string msg)
+        {
+            Logger.LogError(msg);
+        }
+
+    }
+}
---
+++ Customizable Flag/ResizeFlag.cs
@@ -0,0 +1,79 @@
+﻿using BepInEx.Logging;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Customizable_Flag
+{
+    internal class ResizeFlag : MonoBehaviour
+    {
+        Texture2D flag;
+        SkinnedMeshRenderer meshRenderer;
+        string fileLocation = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+        void Awake () 
+        {
+            StartCoroutine(LoadFlagImage());
+        }
+
+        IEnumerator LoadFlagImage()
+        {
+            Plugin.Instance.log("test 1");
+            DirectoryInfo directory = new DirectoryInfo(fileLocation + "\\Flag");
+            Plugin.Instance.log("directory found");
+
+            FileInfo[] files = directory.GetFiles();
+            if (files.Length == 0)
+            {
+                Plugin.Instance.log("No files found in directory.");
+                yield break;
+            }
+
+            // Create the UnityWebRequest
+            UnityWebRequest www = UnityWebRequestTexture.GetTexture("file://" + files[0].FullName);
+
+            // Send the request and wait for it to complete
+            yield return www.SendWebRequest();
+
+            // Check for errors
+            if (www.result != UnityWebRequest.Result.Success)
+            {
+                Plugin.Instance.log("Failed to load image: " + www.error);
+                yield break;
+            }
+
+            // Get the texture from the request
+            Texture2D downloadedTexture = DownloadHandlerTexture.GetContent(www);
+            Plugin.Instance.log("assigned image");
+
+            // Debugging: Log the width and height of the downloaded texture
+            Plugin.Instance.log("Downloaded texture width: " + downloadedTexture.width);
+            Plugin.Instance.log("Downloaded texture height: " + downloadedTexture.height);
+
+            // Ensure the texture uses the image's resolution
+            flag = new Texture2D(downloadedTexture.width, downloadedTexture.height, downloadedTexture.format, false);
+            flag.SetPixels(downloadedTexture.GetPixels());
+            flag.Apply();
+
+            // Debugging: Log the width and height of the new texture
+            Plugin.Instance.log("New texture width: " + flag.width);
+            Plugin.Instance.log("New texture height: " + flag.height);
+
+            // Get the SkinnedMeshRenderer and set the texture and scale
+            meshRenderer = transform.GetChild(0).GetComponent<SkinnedMeshRenderer>();
+            float aspectRatio = (float)flag.width / flag.height;
+            transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z * aspectRatio);
+            transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, aspectRatio / 100f);
+            meshRenderer.material.mainTexture = flag;
+            Plugin.Instance.log("everything is working");
+        }
+
+    }
+}
